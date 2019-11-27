ca3e72d17bb2256e283f43f8f5b28ed798230096
alulab14 <alulab14@inf.pucp.edu.pe>
2019-11-27 11:05:11 -0500

    no se uqe carajo pasa con las fechas, falta modificar que no sea nombre sino ID Usuario y ver eso de las fechas que sale null aun cuando ya le indique que fecha

--- TA_GesBib_Cliente/TA_GesBib_Cliente/frmGenerarReportes.Designer.cs
+++ TA_GesBib_Cliente/TA_GesBib_Cliente/frmGenerarReportes.Designer.cs
@@ -204,6 +204,7 @@
             this.button1.Text = "DESCARGAR";
             this.button1.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
             this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // groupBox1
             // 
--- TA_GesBib_Cliente/TA_GesBib_Cliente/frmGenerarReportes.cs
+++ TA_GesBib_Cliente/TA_GesBib_Cliente/frmGenerarReportes.cs
@@ -20,27 +20,37 @@ namespace TA_GesBib_Cliente
 
         private void guardarReporteInasistencias_FileOk(object sender, CancelEventArgs e)
         {
-            byte[] arreglo = Program.DBController.generarReporteInasistencias(Convert.ToInt32(textBox1.Text), 
-                                Convert.ToDateTime(dateTimePicker5.Value), Convert.ToDateTime(dateTimePicker2.Value) ); 
-
-            File.WriteAllBytes(guardarReporteInasistencias.FileName + ".pdf", arreglo);
-            //mensaje final
-            MessageBox.Show("Se descargo correctamente", "Mensaje", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            
         }
 
         private void button4_Click(object sender, EventArgs e)
         {
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                byte[] arreglo = Program.DBController.generarReporteInasistencias(Convert.ToInt32(textBox1.Text),
+                                    Convert.ToDateTime(dateTimePicker5.Value), Convert.ToDateTime(dateTimePicker2.Value));
 
+                File.WriteAllBytes(guardarReporteInasistencias.FileName + ".pdf", arreglo);
+                //mensaje final
+                MessageBox.Show("Se descargo correctamente", "Mensaje", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
         }
 
         private void saveFileDialog1_FileOk(object sender, CancelEventArgs e)
         {
-            byte[] arreglo = Program.DBController.generarReporteHorasTrabajadas(Convert.ToInt32(textBox3.Text),
-                                Convert.ToDateTime(dateTimePicker4.Value), Convert.ToDateTime(dateTimePicker1.Value));
+            
+        }
 
-            File.WriteAllBytes(guardarReporteInasistencias.FileName + ".pdf", arreglo);
-            //mensaje final
-            MessageBox.Show("Se descargo correctamente", "Mensaje", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK) {
+                byte[] arreglo = Program.DBController.generarReporteHorasTrabajadas(Convert.ToInt32(textBox3.Text),
+                                    Convert.ToDateTime(dateTimePicker4.Value), Convert.ToDateTime(dateTimePicker1.Value));
+
+                File.WriteAllBytes(saveFileDialog1.FileName + ".pdf", arreglo);
+                //mensaje final
+                MessageBox.Show("Se descargo correctamente", "Mensaje", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
         }
     }
 }
